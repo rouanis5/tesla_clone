b390e6e0c534f132e9450efbacdbb28c73a3f4ed
rouanis5 <ouanis42@gmail.com>
2022-08-15 03:38:17 +0100

    + sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,67 @@
 import React from 'react'
 import './styles/tailwind.css'
+import Section from './components/Section'
+
+const model3Img = new URL('./images/model-3.jpg', import.meta.url).href
+const modelYImg = new URL('./images/model-y.jpg', import.meta.url).href
+const modelSImg = new URL('./images/model-s.jpg', import.meta.url).href
+const modelXImg = new URL('./images/model-x.jpg', import.meta.url).href
+const solarPanelImg = new URL('./images/solar-panel.jpg', import.meta.url).href
+const solarRoofImg = new URL('./images/solar-roof.jpg', import.meta.url).href
+const accessoriesImg = new URL('./images/accessories.jpg', import.meta.url).href
 
 function App() {
-  return <div>App</div>
+  return (
+    <div>
+      <Section
+        img={model3Img}
+        title="model 3"
+        description="order online for touchless delivery"
+        leftBtnText="custom order"
+        rightBtnText="existing inventory"
+      />
+      <Section
+        img={modelYImg}
+        title="model Y"
+        description="order online for touchless delivery"
+        leftBtnText="custom order"
+        rightBtnText="existing inventory"
+      />
+      <Section
+        img={modelSImg}
+        title="model S"
+        description="order online for touchless delivery"
+        leftBtnText="custom order"
+        rightBtnText="existing inventory"
+      />
+      <Section
+        img={modelXImg}
+        title="model X"
+        description="order online for touchless delivery"
+        leftBtnText="custom order"
+        rightBtnText="existing inventory"
+      />
+      <Section
+        img={solarPanelImg}
+        title="solar panels"
+        description="Lowest Cost Solar Panels in America"
+        leftBtnText="order now"
+        rightBtnText="learn more"
+      />
+      <Section
+        img={solarRoofImg}
+        title="solar roof"
+        description="Produce Clean Energy From Your Roof"
+        leftBtnText="order now"
+        rightBtnText="learn more"
+      />
+      <Section
+        img={accessoriesImg}
+        title="accessories"
+        leftBtnText="shop now"
+      />
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Section.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+function Section({ img, title, description, leftBtnText, rightBtnText }) {
+  return (
+    <div
+      className="scale- flex min-h-screen flex-col justify-around overflow-hidden bg-cover bg-center text-center"
+      style={{ backgroundImage: `url(${img})` }}
+    >
+      <div>
+        <h2 className="text-5xl capitalize text-black ">{title}</h2>
+        <p className="text-lg capitalize">{description}</p>
+      </div>
+      <div className="container mx-auto grid max-w-lg grid-cols-1 justify-center gap-5 px-5 sm:grid-cols-2">
+        <Button text={leftBtnText} />
+        {rightBtnText && <Button text={rightBtnText} isLight={true} />}
+      </div>
+    </div>
+  )
+}
+
+const lightBtn = 'bg-white text-gray-700 hover:bg-gray-100'
+const darkBtn = 'bg-gray-700 text-white hover:bg-gray-800'
+function Button({ text, isLight }) {
+  return (
+    <button
+      type="button"
+      className={`rounded-full py-2 px-5 text-sm uppercase shadow-lg transition-colors sm:py-3 ${
+        isLight ? lightBtn : darkBtn
+      }`}
+    >
+      {text}
+    </button>
+  )
+}
+
+export default Section
