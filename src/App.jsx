14b974e5601d61b8ae83b56ebfe8e4ed096fff8d
rouanis5 <ouanis42@gmail.com>
2022-08-15 20:24:20 +0100

    create a loop to generate hero sections

--- src/App.jsx
+++ src/App.jsx
@@ -2,64 +2,68 @@ import React from 'react'
 import './styles/tailwind.css'
 import Section from './components/Section'
 
-const model3Img = new URL('./images/model-3.jpg', import.meta.url).href
-const modelYImg = new URL('./images/model-y.jpg', import.meta.url).href
-const modelSImg = new URL('./images/model-s.jpg', import.meta.url).href
-const modelXImg = new URL('./images/model-x.jpg', import.meta.url).href
-const solarPanelImg = new URL('./images/solar-panel.jpg', import.meta.url).href
-const solarRoofImg = new URL('./images/solar-roof.jpg', import.meta.url).href
-const accessoriesImg = new URL('./images/accessories.jpg', import.meta.url).href
-
+const sections = [
+  {
+    img: new URL('./images/model-3.jpg', import.meta.url).href,
+    title: 'model 3',
+    description: 'order online for touchless delivery',
+    leftBtn: 'custom order',
+    rightBtn: 'existing inventory',
+  },
+  {
+    img: new URL('./images/model-y.jpg', import.meta.url).href,
+    title: 'model Y',
+    description: 'order online for touchless delivery',
+    leftBtn: 'custom order',
+    rightBtn: 'existing inventory',
+  },
+  {
+    img: new URL('./images/model-s.jpg', import.meta.url).href,
+    title: 'model S',
+    description: 'order online for touchless delivery',
+    leftBtn: 'custom order',
+    rightBtn: 'existing inventory',
+  },
+  {
+    img: new URL('./images/model-x.jpg', import.meta.url).href,
+    title: 'model X',
+    description: 'order online for touchless delivery',
+    leftBtn: 'custom order',
+    rightBtn: 'existing inventory',
+  },
+  {
+    img: new URL('./images/solar-panel.jpg', import.meta.url).href,
+    title: 'solar panels',
+    description: 'Lowest Cost Solar Panels in America',
+    leftBtn: 'order now',
+    rightBtn: 'learn more',
+  },
+  {
+    img: new URL('./images/solar-roof.jpg', import.meta.url).href,
+    title: 'solar roof',
+    description: 'Produce Clean Energy From Your Roof',
+    leftBtn: 'order now',
+    rightBtn: 'learn more',
+  },
+  {
+    img: new URL('./images/accessories.jpg', import.meta.url).href,
+    title: 'accessories',
+    leftBtn: 'shop now',
+  },
+]
 function App() {
   return (
     <div>
-      <Section
-        img={model3Img}
-        title="model 3"
-        description="order online for touchless delivery"
-        leftBtnText="custom order"
-        rightBtnText="existing inventory"
-      />
-      <Section
-        img={modelYImg}
-        title="model Y"
-        description="order online for touchless delivery"
-        leftBtnText="custom order"
-        rightBtnText="existing inventory"
-      />
-      <Section
-        img={modelSImg}
-        title="model S"
-        description="order online for touchless delivery"
-        leftBtnText="custom order"
-        rightBtnText="existing inventory"
-      />
-      <Section
-        img={modelXImg}
-        title="model X"
-        description="order online for touchless delivery"
-        leftBtnText="custom order"
-        rightBtnText="existing inventory"
-      />
-      <Section
-        img={solarPanelImg}
-        title="solar panels"
-        description="Lowest Cost Solar Panels in America"
-        leftBtnText="order now"
-        rightBtnText="learn more"
-      />
-      <Section
-        img={solarRoofImg}
-        title="solar roof"
-        description="Produce Clean Energy From Your Roof"
-        leftBtnText="order now"
-        rightBtnText="learn more"
-      />
-      <Section
-        img={accessoriesImg}
-        title="accessories"
-        leftBtnText="shop now"
-      />
+      {sections.map((el) => (
+        <Section
+          key={el.title}
+          img={el.img}
+          title={el.title}
+          description={el.description}
+          leftBtnText={el.leftBtn}
+          rightBtnText={el.rightBtn}
+        />
+      ))}
     </div>
   )
 }
