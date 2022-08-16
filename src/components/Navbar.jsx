bd81b9e4b5165ab6f8541ac5edc12cb91e7e38a1
rouanis5 <ouanis42@gmail.com>
2022-08-16 20:56:22 +0100

    + navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,124 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
+import { MdExitToApp } from 'react-icons/md'
+
+const logo = new URL('../images/logo.svg', import.meta.url).href
+
+const links = [
+  { breakpoint: 'xl', text: 'Model S', link: '/#' },
+  { breakpoint: 'xl', text: 'Model 3', link: '/#' },
+  { breakpoint: 'xl', text: 'Model X', link: '/#' },
+  { breakpoint: 'xl', text: 'Model Y', link: '/#' },
+  { breakpoint: 'xl', text: 'Solar roof', link: '/#' },
+  { breakpoint: 'xl', text: 'Solar panels', link: '/#' },
+  { breakpoint: 'sm', text: 'shop', link: '/#' },
+  { breakpoint: 'sm', text: 'account', link: '/#' },
+]
+
+function Navbar() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <nav className="fixed top-0 right-0 left-0">
+      <div className="container mx-auto flex items-center justify-between px-5 pt-5">
+        <div>
+          <a href="/logo">
+            <img src={logo} alt="tesla log" />
+          </a>
+        </div>
+        <div className="hidden xl:block">
+          {links.map(
+            ({ text, link, breakpoint }) =>
+              breakpoint === 'xl' && <Link key={text} text={text} link={link} />
+          )}
+        </div>
+        <div>
+          <div className="hidden sm:inline-block">
+            {links.map(
+              ({ text, link, breakpoint }) =>
+                breakpoint === 'sm' && (
+                  <Link key={text} text={text} link={link} />
+                )
+            )}
+          </div>
+          <button
+            type="button"
+            className="relative inline-block rounded-xl px-4 py-2 capitalize before:absolute before:inset-0 before:-z-10 before:rounded-lg 
+            before:bg-white/30 before:shadow-lg before:backdrop-blur-sm before:transition-opacity hover:before:opacity-100 before:sm:opacity-0"
+            onClick={() => {
+              setOpen(true)
+            }}
+          >
+            menu
+          </button>
+        </div>
+      </div>
+      <AnimatePresence>
+        {open && (
+          <>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{
+                duration: 0.5,
+              }}
+              onClick={() => {
+                setOpen(false)
+              }}
+              className="fixed inset-0 bg-gray-600/30 backdrop-blur-sm"
+            />
+            <motion.div
+              initial={{ transform: 'translateX(100%)', opacity: 0 }}
+              animate={{ transform: 'translateX(0%)', opacity: 1 }}
+              exit={{ transform: 'translateX(100%)', opacity: 0 }}
+              transition={{
+                duration: 0.5,
+              }}
+              className="fixed top-0 right-0 bottom-0 flex w-72 
+                flex-col gap-3 overflow-y-auto bg-gray-200 px-8 py-5 shadow-lg"
+            >
+              <button
+                type="button"
+                className="sticky top-0 ml-auto rounded-full p-4 text-2xl before:absolute before:inset-0 before:-z-10 before:rounded-full before:bg-white before:opacity-0 before:shadow-lg before:transition-opacity hover:before:opacity-30 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2"
+                onClick={() => {
+                  setOpen(false)
+                }}
+              >
+                <MdExitToApp />
+              </button>
+              {links.map(({ text, link }) => (
+                <Link
+                  key={text}
+                  text={text}
+                  link={link}
+                  onClick={() => {
+                    setOpen(false)
+                  }}
+                />
+              ))}
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
+    </nav>
+  )
+}
+
+function Link({ text, link, onClick }) {
+  return (
+    <a
+      className="relative inline-block rounded-xl px-4 py-2 capitalize before:absolute before:inset-0 before:-z-10
+      before:rounded-lg before:bg-white/30 before:opacity-0 before:shadow-lg before:backdrop-blur-sm before:transition-opacity hover:before:opacity-100
+      focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-offset-2"
+      href={link}
+      onClick={onClick}
+    >
+      <span className="">{text}</span>
+    </a>
+  )
+}
+
+export default Navbar
